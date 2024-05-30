d0711a60eeff901264d3139a63b22aec9f926200
Gonzalo Rosano <rosanogonzalo@gmail.com>
2024-05-30 19:14:00 -0300

    solo cambios en el js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,13 @@
 import express from "express";
-
+//import logger from "logger";
+const port = process.env.PORT ?? 3202;
 const app = express();
+//app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("<h2> titulp </h2>");
 });
+console.log(port);
+//app.listen(port, () => {
+// console.log(`server runing on port ${port}`);
+//});
