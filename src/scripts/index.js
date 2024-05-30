24f6dd0bec7d1d02581fb695d83a01a8f2dde110
Gonzalo Rosano <gonzalo.rosano@quanam.com>
2024-05-30 19:30:07 -0300

    Instalacion de socket.io y creación de servidor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,19 @@
 import express from "express";
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
 //import logger from "logger";
-const port = process.env.PORT ?? 3202;
+const port = process.env.PORT ?? 3000;
 const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 //app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   res.send("<h2> titulp </h2>");
 });
 console.log(port);
-//app.listen(port, () => {
-// console.log(`server runing on port ${port}`);
-//});
+app.listen(port, () => {
+  console.log(`server runing on port ${port}`);
+});
